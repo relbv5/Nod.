7ec7d2f3d1cf09fd1f84b81633eb46f917a15291
Walker <dlw7wb@umsystem.edu>
2020-02-23 02:36:34 -0600

    First commit

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-
+import { MenuController } from '@ionic/angular';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  isMenuOpen:boolean = false;
 
-  constructor() {}
+  constructor() { }
+
+ setMenu(){
+   this.isMenuOpen = !this.isMenuOpen;
+   console.log(this.isMenuOpen)
+ }
+
+ openWindow(url){
+  window.open(url)
+ }
 
 }
